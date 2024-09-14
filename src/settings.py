dbf8314fb5622a185e1313ece71faa226656fa9a
Jack Dinsmore <jtd@stanford.edu>
2024-09-14 13:08:22 -0700

    Updated settings

--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,12 @@
-NN_SIGMA_PARALLEL_SCALE = 1.13224018
-NN_KURT_SCALE = 0.93713909
+NN_SIGMA_PARALLEL_SCALE = 1.09686156
+NN_KURT_SCALE = 0.91136976
 
-MOM_SIGMA_PARALLEL_SCALE = 1.01318019
-MOM_KURT_SCALE = 1.04007718
+MOM_SIGMA_PARALLEL_SCALE = 1.07266766
+MOM_KURT_SCALE = 0.86951316
 
 DATA_DIRECTORIES = [
     "/Users/jtd/Documents/research/ixpepl/data",
     "/Users/jtd/Documents/research/ixpepl/data/l2-only",
-    "/Users/jtd/Documents/research/ixpepl/niccolo-test",
 ]
 LEAKAGE_DATA_DIRECTORY = '/'.join(__file__.split('/')[:-2]) + "/data"
 GROUND_PSF_DIRECTORY = "../../data/ground-psfs"
